ae098e80ceac44b58327acb42639d8a774a21528
newfbin <3522088471@qq.com>
2025-03-04 10:02:19 +0800

    部署

--- link-interview-frontend/src/components/GlobalFooter/index.tsx
+++ link-interview-frontend/src/components/GlobalFooter/index.tsx
@@ -12,8 +12,8 @@ export default function GlobalFooter() {
     <div className="global-footer">
       <div>© {currentYear} 面试刷题平台</div>
       <div>
-        <a href="https://www.code-nav.cn" target="_blank">
-          作者：编程导航 - 程序员鱼皮
+        <a href="https://github.com/newfbin" target="_blank">
+          作者：newfbin
         </a>
       </div>
     </div>
