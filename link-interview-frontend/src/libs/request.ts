f500cf0c9030dca21db7df5100de2e3f6895d03f
newfbin <3522088471@qq.com>
2025-03-04 09:36:24 +0800

    部署

--- link-interview-frontend/src/libs/request.ts
+++ link-interview-frontend/src/libs/request.ts
@@ -3,9 +3,9 @@ import axios, {CreateAxiosDefaults} from "axios";
 // 创建 Axios 实例
 // 区分开发和生产环境
 const DEV_BASE_URL = "http://localhost:8101";
-const PROD_BASE_URL = "http://xx.xx.xx.xx";
+const PROD_BASE_URL = "http://101.34.58.141";
 const myAxios = axios.create({
-  baseURL: DEV_BASE_URL,
+  baseURL: PROD_BASE_URL,
   timeout: 10000,
   withCredentials: true,
 } as CreateAxiosDefaults);
